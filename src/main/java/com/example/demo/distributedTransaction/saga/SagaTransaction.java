7b2407ade213763ac5a2ea8b417d3bd4cbeeec5f
冯武康 <fengwukang@logwirecloud.com>
2024-10-19 20:44:42 +0800

    complete processing properties

--- src/main/java/com/example/demo/distributedTransaction/saga/SagaTransaction.java
+++ src/main/java/com/example/demo/distributedTransaction/saga/SagaTransaction.java
@@ -1,8 +1,29 @@
 package com.example.demo.distributedTransaction.saga;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+
 public class SagaTransaction {
 
     private Long xid;
 
+    private List<Runnable> rollBackChain = new ArrayList<>();
 
+    public Long getXid() {
+        return xid;
+    }
+
+    public void setXid(Long xid) {
+        this.xid = xid;
+    }
+
+    public List<Runnable> getRollBackChain() {
+        return rollBackChain;
+    }
+
+    public void setRollBackChain(List<Runnable> rollBackChain) {
+        this.rollBackChain = rollBackChain;
+    }
 }
